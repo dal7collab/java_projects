d37953ea3063d2e6759587d70942e88f11a92a68
dal7collab <scecourse2014@gmail.com>
2020-11-20 20:56:58 +0200

    in progress ...

--- Garage/src/_scenarios_/Buildings_T.java
+++ Garage/src/_scenarios_/Buildings_T.java
@@ -1,9 +1,29 @@
 package _scenarios_;
 
+import buildings.Address;
+import buildings.Garage;
+import enums.Education;
+import enums.Level;
+import enums.Rank;
+import enums.Specialization;
+import people.Master;
+import people.Worker;
+
 public class Buildings_T {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Address a = new Address("Beer Sheva", "Trumpeldor 10");
+		Garage g = new Garage("Alufot20", a);
+		System.out.println(g);
+		
+		Worker w1 = new Master("Klara", Education.BTECH, Rank.Senior, Specialization.Painting, Level.Moderate, Level.High);
+		Worker w2 = new Master("Viola", Education.BSC,   Rank.Senior, Specialization.Engines,  Level.High,     Level.Moderate);
+		Worker w3 = new Master("Lea",   Education.BSC,   Rank.Junior, Specialization.Engines,  Level.Moderate, Level.Moderate);
+		
+		Worker[] t = {w1, w2, w3};
+		g.setTeam(t);	
+		System.out.println(g);
+
 
 	}
 
--- Garage/src/_scenarios_/Vehicles_T.java
+++ Garage/src/_scenarios_/Vehicles_T.java
@@ -1,5 +1,23 @@
 package _scenarios_;
 
-public class Vehicles_T {
+import enums.Body;
+import enums.Brand;
+import enums.Engine;
+import vehicles.Car;
+import vehicles.Truck;
+import vehicles.Vehicle;
 
+public class Vehicles_T {
+	public static void main(String[] args) {
+		Vehicle v1 = new Car(10200409,   Brand.Hiundai, Body.Sedan, Engine.Gasoline);		
+		Vehicle v2 = new Car(20010087,   Brand.Kia,     Body.Sedan, Engine.Gasoline);
+		Vehicle v3 = new Car(56723412,   Brand.Opel,    Body.Sedan, Engine.Diesel);
+		Vehicle t1 = new Truck(56723412, Brand.Opel,    2000, 5);
+
+		Vehicle[] a = {v1, v2, v3, t1};
+		for (Vehicle v : a) {
+			System.out.println(v);
+		}
+
+	}
 }
